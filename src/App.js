45a1931b5ab1ad497b8abdc9a32f139780b76d89
snehomoy100 <snehomoy100@gmail.com>
2021-04-12 22:25:15 +0530

    App.js file updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> it's done.
+          My first react app.
         </p>
         <a
           className="App-link"
